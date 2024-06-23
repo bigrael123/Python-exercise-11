dfd15c64205bca0f0b98117e5a3c2a6da5a26cd7
bigrael123 <bigrael-x@hotmail.com>
2024-06-22 22:24:31 -0300

    added a triangle calculation

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 triangleside1 = 60
 triangleside2 = 60
 triangleside3 = 60
-
-if(triangleside2 == triangleside1 and triangleside2 == triangleside3):
-    print("Equilateral")
-elif((triangleside2 == triangleside1 or triangleside2 == triangleside3 or triangleside1 == triangleside3) and (triangleside1 != triangleside2 or triangleside2 !=  triangleside3 or triangleside1 != triangleside3)):
-    print("Isocele")
-elif(triangleside1 != triangleside2 and triangleside2 !=  triangleside3 and triangleside1 != triangleside3):
-    print("Scalene")
+if(triangleside3 + triangleside2 + triangleside1 == 180):
+    if(triangleside2 == triangleside1 and triangleside2 == triangleside3):
+        print("Equilateral")
+    elif((triangleside2 == triangleside1 or triangleside2 == triangleside3 or triangleside1 == triangleside3) and (triangleside1 != triangleside2 or triangleside2 !=  triangleside3 or triangleside1 != triangleside3)):
+        print("Isocele")
+    elif(triangleside1 != triangleside2 and triangleside2 !=  triangleside3 and triangleside1 != triangleside3):
+        print("Scalene")
+else:
+    print("Not a triangle.")
