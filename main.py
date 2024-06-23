43a5ef73a40d0bd87b184279566ecba9e9810f91
bigrael123 <bigrael-x@hotmail.com>
2024-06-22 22:39:07 -0300

    more conditionals to form a triangle

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-triangleside1 = 60
-triangleside2 = 60
-triangleside3 = 60
-if(triangleside3 + triangleside2 + triangleside1 == 180):
+triangleside1 = int(input("Type the size of the triangle side 1: "))
+triangleside2 = int(input("Type the size of the triangle side 2: "))
+triangleside3 = int(input("Type the size of the triangle side 3: "))
+if(triangleside3 + triangleside2 + triangleside1 == 180 and triangleside1 > 0 and triangleside2>0 and triangleside3>0 and triangleside1< triangleside2 + triangleside3 and triangleside2< triangleside3 + triangleside1 and triangleside3< triangleside2+triangleside1):
     if(triangleside2 == triangleside1 and triangleside2 == triangleside3):
         print("Equilateral")
     elif((triangleside2 == triangleside1 or triangleside2 == triangleside3 or triangleside1 == triangleside3) and (triangleside1 != triangleside2 or triangleside2 !=  triangleside3 or triangleside1 != triangleside3)):
